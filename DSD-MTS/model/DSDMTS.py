badb2a59415e13c893d63927cfe3937e07a4268b
huihevv <37570641+huihevv@users.noreply.github.com>
2024-02-08 01:31:30 +0800

    The first submission
    The first submission

---
+++ DSD-MTS/model/DSDMTS.py
@@ -0,0 +1,151 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange, repeat
+from model.embedding import SeriesEmbedding, TimeFeatureEmbedding, CoordEmbedding
+import numpy as np
+
+class DSDMTS(nn.Module):
+    def __init__(self, args):
+        super(DSDMTS, self).__init__()
+        # freq_map = {'q': 1, 'm': 2, 'w': 3, 'd': 6, 'h': 7, 't': 8, 's': 9}
+        # d_inp = freq_map[args.time_features]
+        self.N = args.N_series
+        self.B = args.batch_size
+        self.L = args.seq_len
+        self.H = args.pred_len
+        self.device = args.device
+        self.embed_size = args.embed_size
+        self.latent_size = args.latent_size
+        self.temp = args.temp
+        self.gc_deep = args.gc_deep
+        self.temporal_index = args.temporal_index
+        self.individual = args.individual
+        self.SeriesEmbedding = SeriesEmbedding(self.L, self.N, self.embed_size, self.latent_size, self.individual)
+        self.TimeFeatureEmbedding = TimeFeatureEmbedding(args.time_features, self.embed_size)
+        self.CoordEmbedding = CoordEmbedding(args.n_coords_layers, args.embed_size, args.fourier_feat_size, args.sigma_s)
+        self.map_layer = nn.Linear(self.L + self.H, self.L)
+        self.gate_layer_0 = nn.Linear(self.L, self.N)
+        self.weight_key = nn.Parameter(torch.zeros(size=(self.latent_size, 1)))
+        nn.init.xavier_uniform_(self.weight_key.data, gain=1.414)
+        self.weight_query = nn.Parameter(torch.zeros(size=(self.latent_size, 1)))
+        nn.init.xavier_uniform_(self.weight_query.data, gain=1.414)
+        self.leakyrelu = nn.LeakyReLU(0.2)
+        self.dropout = nn.Dropout(0.5)
+        self.mu_t_layer = nn.Linear(self.embed_size, self.latent_size)
+        self.var_t_layer = nn.Linear(self.embed_size, self.latent_size)
+        self.mlp = nn.Linear((self.gc_deep + 1), 1)
+        self.mu_h_tilde_layer = nn.Linear(self.embed_size, self.latent_size)
+        self.var_h_tilde_layer = nn.Linear(self.embed_size, self.latent_size)
+        self.inference_layer = nn.Linear(self.latent_size, self.latent_size)
+        self.mu_z_t_hat_layer = nn.Linear(self.latent_size, self.latent_size)
+        self.var_z_t_hat_layer = nn.Linear(self.latent_size, self.latent_size)
+        self.pi = 0.5
+        self.decoder_r = nn.Sequential(
+            nn.Linear(self.latent_size, self.embed_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.embed_size, self.L)
+        )
+        self.decoder_f = nn.Sequential(
+            nn.Linear(self.latent_size, self.embed_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.embed_size, self.H)
+        )
+
+    def get_coords(self, L, H):
+        coords = torch.linspace(0, 1, L+H)
+        # reshape
+        return rearrange(coords, 't -> 1 t 1')
+
+    def gumbel_softmax(self, x, temp, hard=True, eps=1e-10):
+        U = torch.rand(x.size()).to(self.device)
+        gumbel_sample = -torch.autograd.Variable(torch.log(-torch.log(U + eps) + eps))
+        gumbel_sample = x + gumbel_sample
+        gumbel_sample = F.softmax(gumbel_sample / temp, dim=-1)
+        y_soft = gumbel_sample
+        if hard:
+            shape = x.size()
+            _, k = y_soft.data.max(-1)
+            y_hard = torch.zeros(*shape).to(self.device)
+            y_hard = y_hard.zero_().scatter_(-1, k.view(shape[:-1] + (1,)), 1.0)
+            y = torch.autograd.Variable(y_hard - y_soft.data) + y_soft
+        else:
+            y = y_soft
+        return y
+    
+    def self_graph_attention(self, x):
+        key = torch.matmul(x, self.weight_key)
+        query = torch.matmul(x, self.weight_query)
+        data = key.repeat(1, 1, self.N).view(x.size(0), self.N * self.N, 1) + query.repeat(1, self.N, 1)
+        data = data.squeeze(2)
+        data = data.view(x.size(0), self.N, -1)
+        data = self.leakyrelu(data)
+        attention = F.softmax(data, dim=2)
+        attention = self.dropout(attention)
+        return attention
+
+    def discrete_graph(self, x, is_eval):
+        learned_graph = self.self_graph_attention(x)
+        learned_graph = (learned_graph + 1) / 2.
+        learned_graph = torch.stack([learned_graph, 1-learned_graph], dim=-1)
+        learned_graph = learned_graph.reshape(self.N * self.N, -1)
+
+        # gumbel softmax
+        if is_eval:
+            adj = self.gumbel_softmax(learned_graph, self.temp, hard=True)
+        else:
+            adj = self.gumbel_softmax(learned_graph, self.temp, hard=True)
+        
+        adj = adj[:, 0].clone().reshape(self.N, -1)
+        mask = torch.eye(self.N, self.N).bool().to(self.device)
+        adj.masked_fill_(mask, 0)
+        return adj
+    
+    def graph_convolution(self, x, adj, gc_deep):
+        H = x
+        H_0 = x.unsqueeze(dim=-1)
+        for _ in range(gc_deep):
+            H = torch.einsum("bnd,nn->bnd", (H, adj))
+            H_0 = torch.cat((H_0, H.unsqueeze(dim=-1)), dim=3)
+        H_0 = self.mlp(H_0).squeeze(dim=3)
+        return H_0
+    
+    def reparameterization(self, mu, var):
+        return (torch.randn_like(torch.exp(0.5 * var)) * torch.exp(0.5 * var) + mu)
+
+    def forward(self, batch_x, time_x, is_eval):
+        if self.temporal_index is True:
+            # time_x [1, L+H, 1] -> [B, L, D]
+            coords = self.get_coords(self.L, self.H).to(self.device)
+            time_x = repeat(self.CoordEmbedding(coords), '1 t d -> b t d', b=time_x.size(0))
+            time_x = self.map_layer(time_x.permute(0, 2, 1)).permute(0, 2, 1)
+        else:
+            # time_x [B, L, Q] -> [B, L, D]
+            time_x = self.TimeFeatureEmbedding(time_x)
+        
+        time_x = self.gate_layer_0(time_x.permute(0, 2, 1)).permute(0, 2, 1)
+        mu_t = self.mu_t_layer(time_x)
+        var_t = self.var_t_layer(time_x)
+        z_t = self.reparameterization(mu_t, var_t)
+
+        h, h_hat, mu_h_hat, var_h_hat = self.SeriesEmbedding(batch_x, time_x)
+        z_hat = self.reparameterization(mu_h_hat, var_h_hat)
+
+        adj = self.discrete_graph(z_hat, is_eval)
+        h_tilde = self.graph_convolution(h_hat, adj, self.gc_deep)
+        mu_h_tilde = self.mu_h_tilde_layer(h_tilde)
+        var_h_tilde = self.var_h_tilde_layer(h_tilde)
+        z_tilde = self.reparameterization(mu_h_tilde, var_h_tilde)
+        z = self.pi * z_hat + (1 - self.pi) * z_tilde
+        mu_z = self.pi * mu_h_hat + (1 - self.pi) * mu_h_tilde
+        var_z = self.pi * var_h_hat + (1 - self.pi) * var_h_tilde
+
+        # variant Dynamic Inference
+        z_t_hat = self.inference_layer(z)
+        mu_z_t_hat = self.mu_z_t_hat_layer(z_t_hat)
+        var_z_t_hat  = self.var_z_t_hat_layer(z_t_hat)
+
+        # decoder
+        y = self.decoder_f[2](self.decoder_f[1](self.decoder_f[0](z))+h).permute(0, 2, 1)
+        x = self.decoder_r[2](self.decoder_r[1](self.decoder_r[0](z))+h).permute(0, 2, 1)
+        return x, y, mu_t, var_t, mu_z_t_hat, var_z_t_hat, mu_z, var_z
---
+++ DSD-MTS/utils/param_parser.py
@@ -0,0 +1,46 @@
+import argparse
+
+def update_args_from_model_params(args, N_series, device):
+    model_params = {
+        'N_series': N_series, 'device': device, # DSD-MTS
+        "embed_type":3,'factor':3,"output_attention":False,'d_model':512,'embed':'timeF','freq':'h',# Informer
+        'dropout':0.05, 'n_heads':8,'d_ff':2048, 'moving_avg':25,'activation':'gelu','e_layers':2, # Autoformer
+        'd_layers':1, 'distil':True, "enc_in":N_series,  "dec_in":N_series, 'c_out':N_series,
+        }
+    model_params.update(vars(args))
+    args = argparse.Namespace(**model_params)
+    return args
+
+def parameter_parser():
+    parser = argparse.ArgumentParser(description="Run non-stationary forecasting task.")
+    parser.add_argument('--seed', type=int, default=2024)
+    parser.add_argument('--output_file', type=str, default='forecast.csv')
+    # forecast
+    parser.add_argument('--seq_len', type=int, default=96)
+    parser.add_argument('--label_len', type=int, default=48)
+    parser.add_argument('--pred_len', type=int, default=96)
+    parser.add_argument('--temporal_index', type=bool, default=True, help='temporal index or timestamp features?')
+    parser.add_argument('--individual', type=bool, default=False, help='parameter sharing or not?')
+    # data
+    parser.add_argument('--data', type=str, default='exchange_rate') 
+    parser.add_argument('--time_features', type=str, default='s', help='Freq for time features encoding')
+    parser.add_argument('--normalise_time_features', type=bool, default=True)
+    parser.add_argument('--scale', type=bool, default=True)
+    parser.add_argument('--scale_method', type=str, default='std', choices=['std', 'min-max'], help="Normalization method")
+    # forecast model
+    parser.add_argument('--fourier_feat_size', type=int, default=4096, help='Fourier Feature Size')
+    parser.add_argument('--n_coords_layers', type=int, default=1, help='Coordinate Embedding Layers')
+    parser.add_argument('--sigma_s', type=list, default=[0.01, 0.1, 1, 5, 10, 20, 50, 100], help='Scale of B')
+    parser.add_argument('--temp', type=float, default=0.5, help='Temperature value for gumbel-softmax')
+    parser.add_argument('--gc_deep', type=int, default=2, help='Depth of Graph Convolution')
+    parser.add_argument('--series_mixing', type=bool, default=False, help='series mixing or independent?')  # DSD-MTS or DSM-MTS?
+    parser.add_argument('--mixing_model', type=str, default='Autoformer')
+    parser.add_argument('--embed_size', type=int, default=128) 
+    parser.add_argument('--latent_size', type=int, default=256) 
+    parser.add_argument('--do_predict', type=bool, default=False, help='whether to predict unseen future data')
+    # optimization
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--patience', type=int, default=5)
+    parser.add_argument('--gpu', type=int, default=1)
+    return parser.parse_args()
