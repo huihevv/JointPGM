50293e824546821bebb61b0f1fd179860e723e67
huihevv <37570641+huihevv@users.noreply.github.com>
2024-06-28 01:12:12 +0800

    upload scripts

---
+++ scripts/exchange_rate/JointPGM.sh
@@ -0,0 +1,57 @@
+model_name=JointPGM
+
+python -u train.py \
+  --seed 2024 \
+  --data exchange_rate \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 48 \
+  --gc_deep 2 \
+  --belta 0.1 \
+  --embed_size 128 \
+  --latent_size 256 \
+  --batch_size 128 \
+  --lr 1e-3 \
+  --gpu 1
+
+python -u train.py \
+  --seed 2024 \
+  --data exchange_rate \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --gc_deep 2 \
+  --belta 0.1 \
+  --embed_size 128 \
+  --latent_size 256 \
+  --batch_size 128 \
+  --lr 1e-3 \
+  --gpu 1
+
+python -u train.py \
+  --seed 2024 \
+  --data exchange_rate \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --gc_deep 2 \
+  --belta 0.1 \
+  --embed_size 128 \
+  --latent_size 256 \
+  --batch_size 128 \
+  --lr 1e-3 \
+  --gpu 1
+
+python -u train.py \
+  --seed 2024 \
+  --data exchange_rate \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --gc_deep 2 \
+  --belta 0.1 \
+  --embed_size 128 \
+  --latent_size 128 \
+  --batch_size 64 \
+  --lr 1e-3 \
+  --gpu 1
