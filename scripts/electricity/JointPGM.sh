aeb0c8f49c75db8240f15b8363d1bf3c1bb2fb9e
huihevv <37570641+huihevv@users.noreply.github.com>
2024-06-28 01:14:11 +0800

    upload scripts

---
+++ scripts/electricity/JointPGM.sh
@@ -0,0 +1,63 @@
+model_name=JointPGM
+
+python -u train.py \
+  --seed 2024 \
+  --data electricity \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 48 \
+  --gc_deep 2 \
+  --belta 1e-5 \
+  --alpha 0.6 \
+  --embed_size 128 \
+  --latent_size 256 \
+  --batch_size 128 \
+  --lr 1e-3 \
+  --gpu 1
+
+python -u train.py \
+  --seed 2024 \
+  --data electricity \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --gc_deep 2 \
+  --belta 0 \
+  --alpha 0.6 \
+  --embed_size 128 \
+  --latent_size 256 \
+  --batch_size 128 \
+  --lr 1e-3 \
+  --gpu 1
+
+python -u train.py \
+  --seed 2024 \
+  --data electricity \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --gc_deep 2 \
+  --belta 0 \
+  --alpha 0.6 \
+  --embed_size 128 \
+  --latent_size 256 \
+  --batch_size 128 \
+  --max_epochs 35 \
+  --lr 1e-3 \
+  --gpu 1
+
+
+python -u train.py \
+  --seed 2024 \
+  --data electricity \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --gc_deep 2 \
+  --belta 1e-5 \
+  --alpha 0.6 \
+  --embed_size 128 \
+  --latent_size 256 \
+  --batch_size 128 \
+  --lr 1e-3 \
+  --gpu 1
